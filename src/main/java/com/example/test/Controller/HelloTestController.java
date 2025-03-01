91da41d366ce22455d8670c2a8693c599a1a5ed1
Abhishekiih <abhiibaghel1011@gmail.com>
2025-03-01 20:42:07 +0530

    put request Added

--- src/main/java/com/example/test/Controller/HelloTestController.java
+++ src/main/java/com/example/test/Controller/HelloTestController.java
@@ -8,25 +8,38 @@ import com.example.test.dto.UserDTO;
 public class HelloTestController {
 
     // GET Request
+    // http://localhost:8080/hello (Hello from BridgeLabz)
     @GetMapping
     public String getHelloMessage() {
         return "Hello from BridgeLabz";
     }
 
     // GET Request with Query Parameter
+    //http://localhost:8080/hello/query?name=Abhishek (Hello Abhishek from BridgeLabz)
     @GetMapping("/query")
     public String getPersonalizedHello(@RequestParam String name) {
         return "Hello " + name + " from BridgeLabz";
     }
 
+    // GET Request with Params as a Path
+    // http://localhost:8080/hello/params/Abhishek (Hello Abhishek from BridgeLabz)
     @GetMapping("/params/{name}")
     public String sayHelloPathVariable(@PathVariable String name){
         return "Hello " + name+" from Bridgelabz";
     }
 
+    // Post Request
+    // http://localhost:8080/hello/post (Hello Abhishek Baghel from BridgeLabz)
     @PostMapping("/post")
     public String sayHelloPost(@RequestBody UserDTO user){
         return "Hello " + user.getFirstName()+" "+user.getLastName()+" from Bridgelabz";
+    }
+
+    // PUT request with query parameter and params as path
+    // http://localhost:8080/hello/put/Abhishek?lastName=Baghel (Hello Abhishek Baghel from BridgeLabz)
+    @PutMapping("/put/{firstName}")
+    public String sayHelloPut(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
     }
 }
 
